3dc2473f9747d8e979669ef4fed9c7733a5ad870
lyc <liangyongchong>
2019-06-20 15:44:28 +0700

    初始化Animation 项目

---
+++ AnimationDemo/app/src/main/java/com/lyc/indonesia/animationdemo/ViewAnimationActivity.kt
@@ -0,0 +1,12 @@
+package com.lyc.indonesia.animationdemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewAnimationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_animation)
+    }
+}
