5a0d534e23a67a3d819b786b65ca9a2d33112fb8
lyc <liangyongchong>
2019-08-09 11:39:04 +0800

    布局动画

--- AnimationDemo/app/src/main/java/com/lyc/indonesia/animationdemo/MainActivity.kt
+++ AnimationDemo/app/src/main/java/com/lyc/indonesia/animationdemo/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.lyc.indonesia.animationdemo.basemotionanimtion.BaseMotionActivity
 import com.lyc.indonesia.animationdemo.frameanimation.FrameAnimationJavaActivity
 import com.lyc.indonesia.animationdemo.frameanimation.FrameAnimationXmlActivity
+import com.lyc.indonesia.animationdemo.layoutanimation.LayoutAnimationActivity
 import com.lyc.indonesia.animationdemo.moveanimation.MoveAnimationActivity
 import com.lyc.indonesia.animationdemo.showorhide.ShowOrHideDemoActivity
 import com.lyc.indonesia.animationdemo.viewanimation.ViewAnimationActivity
@@ -52,6 +53,10 @@ class MainActivity : AppCompatActivity() {
 
         btn_zoom_view.setOnClickListener {
             startActivity(Intent(this, ZoomImageViewActivity::class.java))
+        }
+
+        btn_layout_animation.setOnClickListener {
+            startActivity(Intent(this, LayoutAnimationActivity::class.java))
         }
     }
 }
--- AnimationDemo/app/src/main/java/com/lyc/indonesia/animationdemo/layoutanimation/LayoutAnimationActivity.kt
+++ AnimationDemo/app/src/main/java/com/lyc/indonesia/animationdemo/layoutanimation/LayoutAnimationActivity.kt
@@ -1,12 +1,19 @@
 package com.lyc.indonesia.animationdemo.layoutanimation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lyc.indonesia.animationdemo.R
+import kotlinx.android.synthetic.main.activity_layout_animation.*
 
 class LayoutAnimationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_layout_animation)
+
+        btn_auto_animation.setOnClickListener {
+            startActivity(Intent(this@LayoutAnimationActivity,AutoAnimationLayoutUpdateActivity::class.java))
+        }
     }
 }
