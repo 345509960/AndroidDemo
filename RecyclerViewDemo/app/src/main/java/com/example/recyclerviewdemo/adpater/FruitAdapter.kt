6424ba47716fbdabd2ba846a536f818911d75964
345509960@qq.com <345509960@qq.com>
2019-10-30 11:18:51 +0800

    '简单的应用'

---
+++ RecyclerViewDemo/app/src/main/java/com/example/recyclerviewdemo/adpater/FruitAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.recyclerviewdemo.adpater
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerviewdemo.model.Fruit
+import com.example.recyclerviewdemo.R
+import com.example.recyclerviewdemo.adpater.FruitAdapter.ViewHolder
+
+/**
+ * create by lyc
+ * on 2019/10/30
+ * 描述 水果展示适配器
+ */
+class FruitAdapter(private val mFruitList: List<Fruit>) :
+    RecyclerView.Adapter<ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var fruitImage: ImageView
+        var fruitName: TextView
+
+        init {
+            fruitImage = view.findViewById<View>(R.id.fruit_image) as ImageView
+            fruitName = view.findViewById<View>(R.id.fruitname) as TextView
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.fruit_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        val (name, imageId) = mFruitList[position]
+        holder.fruitImage.setImageResource(imageId)
+        holder.fruitName.text = name
+    }
+
+    override fun getItemCount(): Int {
+        return mFruitList.size
+    }
+}
---
+++ RecyclerViewDemo/app/src/main/java/com/example/recyclerviewdemo/model/Fruit.kt
@@ -0,0 +1,8 @@
+package com.example.recyclerviewdemo.model
+
+/**
+ * create by lyc
+ * on 2019/10/30
+ * 描述 水果类
+ */
+data class Fruit(val name: String, val imageId: Int)
