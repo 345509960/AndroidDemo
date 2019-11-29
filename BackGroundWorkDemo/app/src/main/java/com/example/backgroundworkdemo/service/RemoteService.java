df059d92f4e9304b3363daa2464aaed6c2fc06f4
345509960@qq.com <345509960@qq.com>
2019-11-29 14:20:36 +0800

    'AIDL'

---
+++ BackGroundWorkDemo/app/src/main/java/com/example/backgroundworkdemo/service/RemoteService.java
@@ -0,0 +1,28 @@
+package com.example.backgroundworkdemo.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import com.example.backgroundworkdemo.IRemoteService;
+
+public class RemoteService extends Service {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // Return the interface
+        return binder;
+    }
+
+    private final IRemoteService.Stub binder = new IRemoteService.Stub() {
+
+        public void basicTypes(int anInt, long aLong, boolean aBoolean,
+                               float aFloat, double aDouble, String aString) {
+            // Does nothing
+        }
+    };
+}
